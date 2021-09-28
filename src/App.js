cd4014f2defa70455d1ecd3ac01d34dce77b0e3c
Ajith Gelli <ajithgelli@Ajiths-MacBook-Pro.local>
2021-09-28 18:40:41 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList'
+import AddTodoForm from './components/AddTodoForm'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Registration from './components/Registration'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Registration</Link>
+          </li>
+          <li>
+            <Link to="/todo">Todo</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li> 
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/todo">
+          <TodoList></TodoList>
+        </Route>
+        <Route path="/register">
+          <Registration></Registration>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="/logout">
+          <Logout></Logout>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
